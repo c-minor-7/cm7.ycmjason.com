3f39f71018277e284ca0d1dd52b4bad930b476e3
Jason Yu <me@ycmjason.com>
2018-06-25 20:45:31 +0100

    feat: added analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,10 +13,17 @@ module.exports = {
       });
     },
   },
+  modules: [
+    ['@nuxtjs/google-analytics', { id: 'UA-113352609-4' }],
+  ],
   head: {
+    title: 'Cm7 | Chords from Jason',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    script: [
+      { src: 'https://www.google.com/recaptcha/api.js' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.png' },
